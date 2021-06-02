06d0582f4638c4726556d1f8557d25ea2495478e
Sahin Reja <sahinreja512@gmail.com>
2021-06-02 23:23:17 +0530

    Fisrt Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const data = require('./data.json');
+const app = express();
+app.set('view engine','hbs');
+app.get('/',(req,res)=>{
+    res.render('index.hbs',{item:data})
+})
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+})
