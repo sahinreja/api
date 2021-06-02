aa1279fce849172597875a52d79ed9db2de37419
sahinreja <70487511+sahinreja@users.noreply.github.com>
2021-06-02 23:38:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.set('view engine','hbs');
 app.get('/',(req,res)=>{
     res.render('index.hbs',{item:data})
 })
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+app.listen(5000,()=>{
+    console.log("Server is running on port 5000");
 })
